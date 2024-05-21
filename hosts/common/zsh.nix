4f7222d58dc24a9d9177607dac5e41a1f1373623
Patrick Louis <patrick.louis@linked-planet.com>
2024-05-21 10:16:00 +0200

    Update zsh-config

--- hosts/common/zsh.nix
+++ hosts/common/zsh.nix
@@ -10,54 +10,75 @@
     enableAutosuggestions = true;
     history.size = 10000;
     history.share = true;
+    history.path = "$HOME/.cache/zsh/history";
 
     plugins = [
     ];
 
     initExtraFirst = ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
+    autoload -U colors && colors	# Load colors
+    PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+    stty stop undef		# Disable ctrl-s to freeze terminal.
     '';
 
     initExtra = ''
-      # autoSuggestions config
+    # Basic auto/tab complete:
+    autoload -U compinit
+    zstyle ':completion:*' menu select cache-path $XDG_CACHE_HOME/zsh/zcompcache
+    zmodload zsh/complist
+    compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
+    _comp_options+=(globdots)		# Include hidden files.
 
-      unsetopt correct # autocorrect commands
+    # vi mode
+    bindkey -v
+    bindkey '^X^X' edit-command-line
+    export KEYTIMEOUT=1
 
-      setopt hist_ignore_all_dups # remove older duplicate entries from history
-      setopt hist_reduce_blanks # remove superfluous blanks from history items
-      setopt inc_append_history # save history entries as soon as they are entered
+    # Use vim keys in tab complete menu:
+    bindkey -M menuselect 'h' vi-backward-char
+    bindkey -M menuselect 'k' vi-up-line-or-history
+    bindkey -M menuselect 'l' vi-forward-char
+    bindkey -M menuselect 'j' vi-down-line-or-history
+    bindkey -v '^?' backward-delete-char
 
-      # auto complete options
-      setopt auto_list # automatically list choices on ambiguous completion
-      setopt auto_menu # automatically use menu completion
-      zstyle ':completion:*' menu select # select completions with arrow keys
-      zstyle ':completion:*' group-name "" # group results by category
-      zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
+    # Change cursor shape for different vi modes.
+    function zle-keymap-select () {
+        case $KEYMAP in
+            vicmd) echo -ne '\e[1 q';;      # block
+            viins|main) echo -ne '\e[5 q';; # beam
+        esac
+    }
+    zle -N zle-keymap-select
+    zle-line-init() {
+        zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+        echo -ne "\e[5 q"
+    }
+    zle -N zle-line-init
+    echo -ne '\e[5 q' # Use beam shape cursor on startup.
+    preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
-      #      bindkey '^I' forward-word         # tab
-      #      bindkey '^[[Z' backward-word      # shift+tab
-      #      bindkey '^ ' autosuggest-accept   # ctrl+space
+    # Use lf to switch directories and bind it to ctrl-o
+    lfcd () {
+        tmp="$(mktemp)"
+        lf -last-dir-path="$tmp" "$@"
+        if [ -f "$tmp" ]; then
+            dir="$(cat "$tmp")"
+            rm -f "$tmp" >/dev/null
+            [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+        fi
+    }
+    bindkey -s '^o' 'ranger\n'
+
+    bindkey -s '^a' 'bc -lq\n'
+
+    bindkey -s '^f' 'cd "$(dirname "$(fzf)")"\n'
+
+    bindkey '^[[P' delete-char
+
+    # Edit line in vim with ctrl-e:
+    autoload edit-command-line; zle -N edit-command-line
+    bindkey '^e' edit-command-line
     '';
-
-    oh-my-zsh = {
-      enable = true;
-      # Standard OMZ plugins pre-installed to $ZSH/plugins/
-      # Custom OMZ plugins are added to $ZSH_CUSTOM/plugins/
-      # Enabling too many plugins will slowdown shell startup
-      plugins = [
-        "git"
-        "sudo" # press Esc twice to get the previous command prefixed with sudo https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/sudo
-      ];
-      extraConfig = ''
-        # Display red dots whilst waiting for completion.
-        COMPLETION_WAITING_DOTS="true"
-      '';
-    };
 
     shellAliases = {
       # Overrides those provided by OMZ libs, plugins, and themes.
